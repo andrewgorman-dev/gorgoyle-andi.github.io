df77429ade3dc132fad2429785831416f636a9b3
Ni-Sky <62119134+gorgoyle-andi@users.noreply.github.com>
2021-07-29 21:50:40 +0200

    Update nav-script.js

--- js/nav-script.js
+++ js/nav-script.js
@@ -3,31 +3,26 @@ const navSlide = () => {
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
     
-    burger.addEventListener('click',()=>{
-        // Toggle Nav
+    
+    burger.addEventListener('click', () => {
+        // Toggle nav
         nav.classList.toggle('nav-active');
 
-    // Animate Nav-links
+        // Animate links
         navLinks.forEach((link, index) => {
-            // console.log(index);
             if(link.style.animation){
                 link.style.animation = ''
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.3}s`;
             }
+            
         });
-    // Burger Animation
-    burger.classList.toggle('toggle');
+        // Burger animation
+        burger.classList.toggle('toggle');
     });
 }
 
-navSlide()
-
-
-// Cleaner Code Tip: call functions in here:
-// const navScript = ()=>{
-//     navSlide();
-// }
+navSlide();
 
 
 
